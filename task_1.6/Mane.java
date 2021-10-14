6c7b62ebd62c90645ec0aabf2cd415387b3e99a1
alexsmov <92463762+alexsmov@users.noreply.github.com>
2021-10-14 05:11:08 +0300

    Create Mane.java

---
+++ task_1.6/Mane.java
@@ -0,0 +1,11 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 100;
+        System.out.println(a + 15.0/100.0);
+    }
+}
