bb3eca27333d1d2222da47da7220341659a87819
alexsmov <92463762+alexsmov@users.noreply.github.com>
2021-10-14 05:02:03 +0300

    Create Mane.java

---
+++ task_1.3/Mane.java
@@ -0,0 +1,14 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        String s = "five";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+
+    }
+}
