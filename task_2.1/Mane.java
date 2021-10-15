18e398b437357d11bc53da584690e188bc08b5ba
alexsmov <92463762+alexsmov@users.noreply.github.com>
2021-10-15 15:54:20 +0300

    Create Mane.java

---
+++ task_2.1/Mane.java
@@ -0,0 +1,26 @@
+/*Вывести на экран все возможные комбинации слов "Мама", "Мыла", "Раму".
+Каждую комбинацию вывести с новой строки. Слова не разделять.
+Пример:
+МылаРамуМама
+РамуМамаМыла
+...
+Требования:
+•	Программа должна выводить текст.
+•	Текст в каждой строке должен быть уникален.
+•	Должны быть выведены все возможные комбинации. */
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "Мама";
+        String b= "Мыла";
+        String c= "Раму";
+
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+a+c);
+        System.out.println(b+c+a);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
+
+    }
+}
